b907101ea6974598a65e06a63a3fadcf99780ff7
fir <firman.alqoumy@gmail.com>
2012-01-18 19:08:39 +0700

    okeh commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imigrasi::Application.config.secret_token = '9287b8a1333e9fddecc538b4c72874c5597abfeb4eca4d64fba89d0f3d07ccef4e09f7d706ee125e7ab0d1852b1892bb9a47cd6902d29cc3ba430b2052488e46'
